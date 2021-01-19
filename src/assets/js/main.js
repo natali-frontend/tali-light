75bcd9d1b95b39a98c4b686df4c4d457498bed05
Nataliya Rohochaya <lc4821577@gmail.com>
2021-01-19 19:16:18 +0200

    feature: burger

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log(1)
 });
+
+const header = document.getElementsByClassName("header")[0];
+const burger = document.getElementsByClassName("burger")[0];
+const menu = document.getElementsByClassName("menu")[0];
+burger.addEventListener("click", function () {
+    this.classList.toggle("active");
+    menu.classList.toggle("mobile-menu");
+    header.classList.toggle("header-mobile")
+});
