3d6a2329963575dd2c41e2445e1775d3bda7808a
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-01-21 12:24:10 +0200

    features: finished project

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log(1)
-});
 
-const header = document.getElementsByClassName("header")[0];
-const burger = document.getElementsByClassName("burger")[0];
-const menu = document.getElementsByClassName("menu")[0];
-burger.addEventListener("click", function () {
-    this.classList.toggle("active");
-    menu.classList.toggle("mobile-menu");
-    header.classList.toggle("header-mobile")
+    const header = document.getElementsByClassName("header")[0];
+    const burger = document.getElementsByClassName("burger")[0];
+    const menu = document.getElementsByClassName("menu")[0];
+    burger.addEventListener("click", function () {
+        this.classList.toggle("active");
+        menu.classList.toggle("mobile-menu");
+        header.classList.toggle("header-mobile")
+    });
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 60) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+
+    // Smooth animation
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = anchor.getAttribute("href");
+            const block = document.querySelector(id);
+            burger.classList.toggle("active");
+            menu.classList.toggle("mobile-menu");
+            header.classList.toggle("header-mobile");
+            window.scroll({
+                top: block.offsetTop - 100,
+                left: 0,
+                behavior: 'smooth'
+            });
+        });
+    });
+
 });
