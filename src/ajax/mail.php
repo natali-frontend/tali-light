3d1af22bb3d152899c0eaf3d7a20def3fadd00d1
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-01-28 19:57:25 +0200

    fix: name

--- src/ajax/mail.php
+++ src/ajax/mail.php
@@ -2,7 +2,7 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $subject = "=?utf-8?B?" . base64_encode("Contact us - Dance Mat Typing From KidzType") . "?=";
+    $subject = "=?utf-8?B?" . base64_encode("Contact us -  Tali Light") . "?=";
     $message = "Name: " . $email . "\r\n Message: " . $message . "\r\n";
     $headers = "From: $email\r\nReply-to: $email\r\nContent-Type: text/plain; charset=utf-8\r\n";
 
